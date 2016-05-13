98bcb3ff1ec9348558f1d72e631ca8abc61821a9
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-05-13 15:27:53 -0700

    validation

---
+++ tensor-flow-udacity.test/a_1_p_3.spec.fs
@@ -0,0 +1,28 @@
+﻿module a_1_p_3.spec
+
+open NUnit.Framework
+open FsUnit
+
+open a_1_p_3
+
+[<Test>]
+let ``Render 5 items with randomized range [1..10]`` () =
+        let random = new System.Random(1)
+        let rnd max = random.Next max
+        let permutes = rpermute rnd 10 5 []
+
+        permutes |> should equal [6; 7; 4; 1; 2]
+
+
+[<Test>]
+let ``Render train, test, valid sets with 2 elements, randomized ranges 6, 5, 4`` () =
+
+        let random = new System.Random(1)
+        let limits = [6; 5; 4]
+        let rnd max = random.Next max
+        let permutes = permute rnd limits 2 2 2
+
+        //main pint here : validation set (3d) shouldn't contain elements from training set (1st)
+        //value for each element shouldn't exceed limit from limits
+        permutes |> should equal ([[0; 1]; [3; 2]; [1; 2]], [[5; 2]; [3; 0]; [1; 0]], [[4; 5]; [4; 1]; [3; 0]])
+
--- tensor-flow-udacity/a_1_p_3.fs
+++ tensor-flow-udacity/a_1_p_3.fs
@@ -6,3 +6,28 @@ Another check: we expect the data to be balanced across classes. Verify that.
 Merge and prune the training data as needed. Depending on your computer setup, you might not be able to fit it all in memory, and you can tune train_size as needed. The labels will be stored into a separate array of integers 0 through 9.
 Also create a validation dataset for hyperparameter tuning.
 *)
+
+
+///Generate array of items of length num with random elements from range 0..max
+///Exclued `exclude` elements from generated array
+let rpermute rnd max num (exclude : _ list)=
+    let mutable seen = []
+    while seen.Length < num do
+        let r = rnd max
+        if not (List.contains r seen) && not (List.contains r exclude) then
+            seen <- r::seen
+    seen
+            
+
+
+        
+///trianSize testSize validSize - numbers of each letter in sets
+let permute rnd (limits : int list) trainSize testSize validSize  =     
+    let prm size excl mx  = rpermute rnd mx size excl
+    let train = limits |> List.map (prm trainSize [])
+    let test = limits |> List.map (prm testSize [])
+    let valid = limits |> List.mapi (fun i e -> prm validSize train.[i] e)
+
+    train, test, valid
+
+
