c6531931e122ffa7a638d258b97758212951f2d4
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-07-03 16:35:32 +0300

    heatmap image rotate

--- charts/PointChart.fs
+++ charts/PointChart.fs
@@ -3,11 +3,18 @@
 open RProvider
 open RProvider.graphics
 
+let revImage dim pixels = 
+    pixels 
+    |> Seq.chunkBySize dim
+    |> Seq.rev    
+    |> Seq.collect (fun f -> f)
+    |> Seq.toArray
+
 let showImageBW (pixels: byte array) = 
     
     let width = int <| System.Math.Sqrt( (float) pixels.Length )
     let height = width
-    let fpixels = pixels |> Array.map float
+    let fpixels = pixels |> Array.map float |> revImage width
         
     let mx = namedParams [ "data", box fpixels; "nrow", box width; "ncol", box height; ] |> R.matrix
     namedParams ["x", box mx; "xlab", box ""; "ylab", box ""; "axes", box false] |> R.image |> ignore
