30a357aa68fcd2040d95737142e4aa18e758d0f9
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-07-10 13:01:38 +0300

    config and run images rw via config settings

--- Actors/ML.ReadWriteImages/Program.fs
+++ Actors/ML.ReadWriteImages/Program.fs
@@ -3,24 +3,27 @@
 open Akka.FSharp
 open Akka.FSharp.Spawn
 open Akka.Actor
+open FSharp.Configuration
 
+open DataProccessing.Types
 open ML.Actors.Types
 open ML.Actors.Init
 open ML.Actors.InputActor
 open ML.Actors.Image.ReadWriteCoordinatorActor
 open ML.Actors.Charts.ChartActor
 
+type Settings = AppSettings<"app.config">
 
 [<EntryPoint>]
 let main argv = 
 
-    InitLogger "http://localhost:5341"
+    InitLogger Settings.SeqUri.OriginalString
     
     let system = System.create "Images2DataSetActorSystem" (Configuration.load ())
     let chartActor = spawn system "ChartActor" (ChartActor)    
 
     let rwActor = spawn system "ReadWriteCoordinator" (ReadWriteCoordinatorActor)
-    let inputActor = spawn system "Input" (InputActor rwActor)
+    //let inputActor = spawn system "Input" (InputActor rwActor)
    
     let mainActor = spawn system "main" ( actorOf( fun msg ->
                     
@@ -33,7 +36,11 @@ let main argv =
 
     system.EventStream.Subscribe(mainActor, typedefof<RWMessage>) |> ignore
     
-    inputActor <! Start
+    //inputActor <! Start
+    rwActor <! RWStart {
+        input = DirPath Settings.ML_IMAGES_INPUT_DIR_PATH; 
+        output = Settings.ML_IMAGES_OUTPUT_FILE_PATH
+        }
     
     system.WhenTerminated.Wait()
 
